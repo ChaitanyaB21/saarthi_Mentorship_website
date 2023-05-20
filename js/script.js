7a6ee8aa1cf093c60d5ac9f0b5b9d27d283dfffd
ChaitanyaB21 <113889111+ChaitanyaB21@users.noreply.github.com>
2023-05-20 16:48:32 +0530

    commit after activating the email in the form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 let menu = document.querySelector('#menu');
 let navbar = document.querySelector('.navbar');
 
@@ -11,16 +13,25 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
+
 function sendEmail() {
     Email.send({
-        Host: "smtp.gmail.com",
-        Username: "username",
-        Password: "password",
-        To: 'them@website.com',
-        From: document.getElementById("email").value,
+        SecureToken: "b0d93b31-6151-44c5-8564-3a60df933404",
+        To: 'saarthiyourmentor@gmail.com',
+
+        From: 'saarthiyourmentor@gmail.com',
         Subject: "New contact form enquiry",
-        Body: "Name:" + document.getElementById("name").value + "<br> Email Id:" + document.getElementById("email").value + "<br>Phone Number: " + document.getElementById("phoneNumber").value + "<br>Message: " + document.getElementById("message").value
+        Body: "Name: " + document.getElementById("name").value + "<br>Email Id: " + document.getElementById("email").value + "<br>Phone Number: " + document.getElementById("phoneNumber").value + "<br>Message: " + document.getElementById("message").value
     }).then(
-        message => alert("Message was sent successfully.Thank you for contacting us.You will receive a reply soon")
+        message => alert("Message was sent successfully. Thank you for contacting us. You will receive a reply soon.")
     );
 }
+
+function reset() {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phoneNumber").value = "";
+    document.getElementById("message").value = "";
+    // alert("Message was sent successfully. Thank you for contacting us. You will receive a reply soon.")
+}
+
